cdd6489cff8e6698e4dcf8c4d26bad65089437ca
Roman Slobodchikov <roman@slobodchikov.net>
2022-12-07 20:34:44 +0100

    Day 6

---
+++ Day6/d06p1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  7 20:11:23 2022
+
+@author: romslo
+"""
+
+with open('input.txt') as f:
+    input = f.readlines()
+    
+num_of_char = 4
+
+for code in input:
+    for i in range(len(code)):
+        marker = code[i:num_of_char+i]
+        if len(set(marker)) == len(marker):
+            print(str(i+num_of_char) + " " + marker)
+            break
